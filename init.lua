ba46fe013332c149710821a24cfdb16d11e7cfd9
hoangvietdo <hoangvietdo.bk@gmail.com>
2025-09-11 15:12:25 +0900

    add mini.move

--- init.lua
+++ init.lua
@@ -275,7 +275,14 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  {
+    'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  },
+
+  {
+    'nvim-mini/mini.move',
+    opts = {},
+  },
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -647,7 +654,6 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'clangd',
-        -- 'black',
         'shfmt',
         'pyright',
       })
